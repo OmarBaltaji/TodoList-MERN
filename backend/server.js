8be8d87d476555f49a0b5ecd3f79ed0323fa2c3f
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-15 02:38:58 +0300

    cleanups and adding middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+require('express-async-errors');
+
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const { NotFoundMiddleware, ErrorHandlerMiddleware } = require('./middleware');
+const connectDB = require('./db/connect');
 
 require('dotenv').config(); // Load env variables here
 
@@ -10,19 +13,25 @@ const port = process.env.PORT || 5000;
 app.use(cors()); // skips origin policy and accesses resources from remotehost
 app.use(express.json()) // to parse json
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-});
-
 const itemsRouter = require('./routers/todoItem');
 const listRouter = require('./routers/todoList');
 
-app.use('/items', itemsRouter);
-app.use('/lists', listRouter);
+app.use('/api/v1/items', itemsRouter);
+app.use('/api/v1/lists', listRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+app.use(NotFoundMiddleware);
+app.use(ErrorHandlerMiddleware);
+
+
+const start = async() => {
+    try {
+        await connectDB(process.env.ATLAS_URI);
+        app.listen(port, () => {
+            console.log(`Server is running on port: ${port}`);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();

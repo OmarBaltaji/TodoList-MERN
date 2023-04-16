f622659585f0c1d88083593080f9e543880691b8
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-16 17:42:42 +0300

    cleanup and optimizing Home component and created new Form component for adding/editing records

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from './Button';
+import '../styles/css/form.css';
+
+export default function Form({handleOnSubmit, value, handleOnChange}) {
+ 
+  return (
+    <form>
+      <div className="input-group mb-3 col-md-4">
+        <input 
+            className="ml-5 px-2 form-control"
+            aria-describedby="button-addon2"
+            required
+            value={value}
+            onChange={handleOnChange}
+            placeholder="Please enter a title for your list"
+        />
+        <Button 
+            className="btn-outline-primary border-rounded-end"  
+            onClickHandler={handleOnSubmit}
+            innerText="Add"
+            id="button-addon2"
+        />
+      </div>
+    </form>
+  )
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,17 @@ import React, {useState, useEffect} from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import api from '../api';
 import Button from './Button';
+import Form from './Form';
 
 export default function Home() {
     const [title, setTitle] = useState('');
     const [lists, setLists] = useState([]);
-    const [items, setItems] = useState([]);
-    const [listDeleted, setListDeleted] = useState(false);
-    const [listAdded, setListAdded] = useState(false);
+    const [areListsChanged, setAreListsChanged] = useState(false);
     const history = useHistory();
 
     useEffect(() => {
         getAllLists();
-        setListDeleted(false);
-        setListAdded(false)
-    }, [listDeleted, listAdded]);
+    }, [areListsChanged]);
 
     async function handleOnSubmit(e) {
         e.preventDefault();
@@ -23,7 +20,8 @@ export default function Home() {
 
         try {
             const {data: successMessage} = await api.createList(formData);
-            setListAdded(true);
+            setAreListsChanged(oldValue => !oldValue);
+            setTitle('');
         } catch (err) {
             console.error(err);
         }
@@ -41,14 +39,10 @@ export default function Home() {
     async function deleteList(id) {
         try {
             const {data: successMessage} = await api.deleteList(id);
-            setListDeleted(true);
+            setAreListsChanged(oldValue => !oldValue);
         } catch (err) {
             console.error(err);
         }
-    }
-
-    function handleRedirectToEdit(id) {
-        history.push(`/editlist/${id}`);
     }
 
     function displayAllLists() {
@@ -58,7 +52,7 @@ export default function Home() {
                        return( 
                            <div className="row mb-4" key={index}>
                                 <li className="mt-1"><Link to={`/list/${list._id}`}>{list.title}</Link></li>&emsp;
-                                <Button className="btn-primary" onClickHandler={() => handleRedirectToEdit(list._id)} innerText="Edit" />
+                                <Button className="btn-primary" onClickHandler={() => history.push(`/editlist/${list._id}`)} innerText="Edit" />
                                 <Button className="btn-danger" onClickHandler={() => deleteList(list._id)} innerText="Delete" />
                             </div>
                        );
@@ -72,23 +66,7 @@ export default function Home() {
     return (
         <div className="mt-4">
             <h1 className="mx-auto mb-5" style={{ width:'500px' }}>Welcome to your TodoList!</h1>
-                <div className="input-group mb-3">
-                    <input 
-                        className="ml-5 px-2"
-                        style={{ width:'250px' }}
-                        aria-describedby="button-addon2"
-                        required
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Please enter a title for your list"
-                    />
-                    <Button 
-                        className="btn-outline-primary"  
-                        onClickHandler={(e) => handleOnSubmit(e)}
-                        innerText="Add"
-                        id="button-addon2"
-                    />
-                </div>
+                <Form handleOnChange={(e) => setTitle(e.target.value)} value={title} handleOnSubmit={(e) => handleOnSubmit(e)} />
                 <div className="ml-5">
                     {displayAllLists()}
                 </div>

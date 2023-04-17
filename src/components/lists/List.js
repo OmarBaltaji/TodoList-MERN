b40e6eaf6fbf0572b423da67c89e0c3358afc419
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-17 19:13:24 +0300

    Added icons and displayed a card that is empty which can then be used to add a new list

--- src/components/lists/List.js
+++ src/components/lists/List.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import Button from '../common/Button';
-import { Link } from 'react-router-dom';
+import {checkIfObjEmpty} from '../../utilities';
+import Form from '../common/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-export default function List({list, onEditHandler, onDeleteHandler}) {
-  return (
-    <div className='col-md-3 mb-3 mt-3'>
-        <div className='card'>
-        <Link to={`/list/${list._id}`} className="text-decoration-none">
-            <div className="card-body">
-              <div className='card-title text-center text-dark'>
-                {list.title}
-              </div>
-              <div className='card-text'>
+export default function List({list, onEditHandler, onDeleteHandler, onChangeHandler, titleValue, onSubmitHandler, handleKeyDown}) {
+  const displayNewEmptyList = () => (
+    <Form handleOnChange={onChangeHandler} value={titleValue} handleOnSubmit={onSubmitHandler} handleKeyDown={handleKeyDown} />
+  );
 
-              </div>
-            </div>
-        </Link>
-          <div className='card-footer text-center'>
-            <Button className="btn-primary mr-3" onClickHandler={onEditHandler} innerText="Edit" />
-            <Button className="btn-danger" onClickHandler={onDeleteHandler} innerText="Delete" />
-          </div>
+  const displayExistingList = () => (
+    <>
+      <div className='card--header text-right pt-3 pr-3 text-danger'>
+        <FontAwesomeIcon icon={faTrashCan} onClick={onDeleteHandler} />
+      </div>
+      <div className="card-body" title='Edit'>
+        <div className='card-title text-center text-dark'>
+          {list.title}
         </div>
+        <div className='card-text'>
+          {/* items */}
+        </div>
+      </div>
+      <div className='card-footer text-center'>
+        <Button className="btn-primary mr-3" onClickHandler={onEditHandler} innerText="Edit" />
+      </div>
+    </>
+  )
+
+  return (
+    <div className='col-md-3 my-3'>
+      <div className='card'>
+        {checkIfObjEmpty(list) ? displayNewEmptyList() : displayExistingList()}
+      </div>
     </div>
   );
 }

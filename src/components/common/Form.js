2e7ae199ec666608eb9d3aebdf258d6c2a69dbcb
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-20 00:04:58 +0300

    restyled form and added additional event listeners

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -2,29 +2,28 @@ import React from 'react';
 import Button from './Button';
 import '../../styles/css/form.css';
 
-export default function Form({handleOnSubmit, value, handleOnChange, handleKeyDown, isEdit = false, inputPlaceholder = "Please enter a title for your list"}) {
+export default function Form({handleOnSubmit, value, handleOnChange, handleKeyDown, isEdit = false, inputPlaceholder = "Please enter a title for your list", handleOnBlur}) {
  
   return (
-    <form className='d-flex flex-column justify-content-between h-100'>
-      <div>
-        <input 
-            className="form-control"
-            aria-describedby="button-addon2"
-            required
-            value={value}
-            onChange={handleOnChange}
-            placeholder={inputPlaceholder}
-            onKeyDown={handleKeyDown}
-        />
-      </div>
-      <div>
-        <Button 
-            className="btn-outline-primary border-rounded-end w-100"  
-            onClickHandler={handleOnSubmit}
-            innerText={isEdit ? "Edit" : "Add"}
-            id="button-addon2"
-        />
-      </div>
+    <form className='d-flex justify-content-center align-items-center w-100 h-100'>
+    <div className='input-group w-75'>
+      <input 
+          className="form-control text-center"
+          aria-describedby="button-addon2"
+          required
+          value={value}
+          onChange={handleOnChange}
+          placeholder={inputPlaceholder}
+          onKeyDown={handleKeyDown}
+          onBlur={handleOnBlur}
+      />
+      <Button 
+          className="btn btn-outline-primary border-rounded-end"  
+          onClickHandler={handleOnSubmit}
+          innerText={isEdit ? "Edit" : "Add"}
+          id="button-addon2"
+      />
+    </div>
     </form>
   )
 }

3058fbc636c9cd810ca3d914fc92a2ca36d6b0ec
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-22 13:54:20 +0300

    displaying items directly inside the list card

--- src/components/lists/List.js
+++ src/components/lists/List.js
@@ -3,8 +3,9 @@ import {checkIfObjEmpty} from '../../utilities';
 import Form from '../common/Form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import Item from '../items/Item';
 
-export default function List({list, onDeleteHandler, onChangeHandler, titleValue, onSubmitHandler, handleKeyDown, handleShowTitleForm, handleOnMouseLeave}) {
+export default function List({list, onDeleteHandler, onChangeHandler, titleValue, onSubmitHandler, handleKeyDown, handleShowTitleForm, handleOnMouseLeave, itemOnChangeHandler, itemOnDeleteHandler}) {
   const displayForm = (value, isEdit) => (
     <Form handleOnChange={onChangeHandler} value={value} handleOnSubmit={onSubmitHandler} handleKeyDown={handleKeyDown} isEdit={isEdit} />
   )
@@ -17,20 +18,33 @@ export default function List({list, onDeleteHandler, onChangeHandler, titleValue
     displayForm(list.title, true)
   )
 
-  const displayExistingList = () => (
-    <>
-      <div 
-        className='card-title d-flex justify-content-center align-items-center text-dark h-100 mb-0' 
-        onClick={handleShowTitleForm} 
-        onKeyDown={handleKeyDown}
-      >
-        {list.showTitleForm ? displayEditTitleForm(list) : <span className='card-title-text cursor-pointer'>{list.title}</span>}
-      </div>
-      <div className='card-text'>
-        {/* items */}
-      </div>
-    </>
-  )
+  const displayExistingList = () => {
+    return (
+      <>
+        <div 
+          className='card-title d-flex justify-content-center text-dark h-100 mb-0' 
+          onClick={handleShowTitleForm} 
+          onKeyDown={handleKeyDown}
+        >
+          {list.showTitleForm ? displayEditTitleForm(list) : <span className='card-title-text cursor-pointer pt-5 pb-4'>{list.title}</span>}
+        </div>
+        <div className='card-text'>
+          <ul className="list-group">
+              {list.items && list.items.length > 0 && 
+                list.items.map(item => 
+                  <Item 
+                    item={item} 
+                    onChangeHandler={(e) => itemOnChangeHandler(e, item._id)} 
+                    onDeleteHandler={(e) => itemOnDeleteHandler(e, item._id)} 
+                    key={item._id}   
+                  />
+                )
+              }
+            </ul>
+        </div>
+      </>
+    )
+  }
 
   return (
     <div className='col-md-3 my-3'>

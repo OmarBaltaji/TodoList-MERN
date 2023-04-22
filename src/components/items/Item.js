5f1b1bbd6c173f832227a16dd352d92644269722
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-22 13:54:56 +0300

    reworked items, switched buttons with icons

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import Button from '../common/Button';
-import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function Item ({item, onChangeHandler, onDeleteHandler}) {
-  const history = useHistory();
 
   return (
-    <div  className="d-flex align-items-center mb-3">
-        <li className='mr-3'>{item.name}</li>
-        <input 
-            className="mr-4" 
-            type="checkbox" 
-            onChange={onChangeHandler}
-            checked={item.done}  
-        />
-        <Button className="btn-primary mr-3" onClickHandler={() => history.push(`/edititem/${item._id}`)} innerText="Edit" />
-        <Button className="btn-danger" onClickHandler={onDeleteHandler} innerText="Delete" />
+    <div className="list-group-item d-flex">
+        <span className='mr-3'>{item.name}</span>
+        <div className='mr-auto position-relative hoverable'>
+          <input className='item-check-box opacity-0 cursor-pointer' type="checkbox" onChange={onChangeHandler} checked={item.done}  />
+          <FontAwesomeIcon icon={faCheck} className={item.done ? 'text-success' : ''} />
+        </div>
+        {/* <Button className="btn-primary mr-3" onClickHandler={() => history.push(`/edititem/${item._id}`)} innerText="Edit" /> */}
+        <FontAwesomeIcon className='delete-icon cursor-pointer text-danger' icon={faXmark} onClick={onDeleteHandler} />
     </div>
   )
 }

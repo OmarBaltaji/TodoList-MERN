60b61056d03b136cea64e575ae25ecd96afd697d
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-23 17:25:28 +0300

    added the ability to edit item name on the fly, the input will be shown in case the item name was clicked

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -4,21 +4,30 @@ import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { checkIfObjEmpty } from '../../utilities';
 import Form from '../common/Form';
 
-export default function Item ({item, onChangeHandler, onDeleteHandler, onSubmitHandler, handleKeyDown, itemNameValue}) {
+export default function Item ({item, onChangeHandler, onDeleteHandler, onSubmitHandler, handleKeyDown, onCheckHandler, itemNameValue, handleShowItemNameForm}) {
+  const displayForm = (isEdit = false) => (
+    <Form handleOnChange={onChangeHandler} value={itemNameValue} handleOnSubmit={onSubmitHandler} handleKeyDown={handleKeyDown} isEdit={isEdit} />
+  )
+
+  const displayItem = () => (
+    <>
+      <span className={'mr-3 hoverable cursor-pointer ' + (item.done ? 'text-decoration-line-through' : '')} onClick={handleShowItemNameForm}>{item.name}</span>
+      <div className='mr-auto position-relative hoverable'>
+        <input className='item-check-box opacity-0 cursor-pointer' type="checkbox" onChange={onCheckHandler} checked={item.done}  />
+        <FontAwesomeIcon icon={faCheck} className={'hoverable ' + (item.done ? 'text-success' : '')} />
+      </div>
+      {/* <Button className="btn-primary mr-3" onClickHandler={() => history.push(`/edititem/${item._id}`)} innerText="Edit" /> */}
+      <FontAwesomeIcon className='delete-icon cursor-pointer text-danger hoverable' icon={faXmark} onClick={onDeleteHandler} />
+    </>
+  )
+
+
   return (
     <div className="list-group-item d-flex align-items-center">
       {checkIfObjEmpty(item) ? 
-        <Form handleOnChange={onChangeHandler} value={item.name ?? itemNameValue} handleOnSubmit={onSubmitHandler} handleKeyDown={handleKeyDown} />
+        displayForm()
       :
-        <>
-          <span className={'mr-3 ' + (item.done ? 'text-decoration-line-through' : '')}>{item.name}</span>
-          <div className='mr-auto position-relative hoverable'>
-            <input className='item-check-box opacity-0 cursor-pointer' type="checkbox" onChange={onChangeHandler} checked={item.done}  />
-            <FontAwesomeIcon icon={faCheck} className={'hoverable ' + (item.done ? 'text-success' : '')} />
-          </div>
-          {/* <Button className="btn-primary mr-3" onClickHandler={() => history.push(`/edititem/${item._id}`)} innerText="Edit" /> */}
-          <FontAwesomeIcon className='delete-icon cursor-pointer text-danger hoverable' icon={faXmark} onClick={onDeleteHandler} />
-        </>
+        (item.showNameForm ? displayForm(true) : displayItem())
       }
     </div>
   )

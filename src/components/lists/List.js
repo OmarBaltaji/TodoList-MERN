9fae460d82a8a20dfc8f0404de6b4eed03160e1a
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-23 15:40:06 +0300

    added new empty item display with events related to it

--- src/components/lists/List.js
+++ src/components/lists/List.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {checkIfObjEmpty} from '../../utilities';
 import Form from '../common/Form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import Item from '../items/Item';
 
-export default function List({list, onDeleteHandler, onChangeHandler, titleValue, onSubmitHandler, handleKeyDown, handleShowTitleForm, handleOnMouseLeave, itemOnChangeHandler, itemOnDeleteHandler}) {
+export default function List({list, onDeleteHandler, onChangeHandler, titleValue, onSubmitHandler, handleKeyDown, handleShowTitleForm, handleOnMouseLeave, itemOnChangeHandler, itemOnDeleteHandler, addNewItemHandler, itemOnSubmitHandler, itemOnCheckHandler, itemNameValue}) {
   const displayForm = (value, isEdit) => (
     <Form handleOnChange={onChangeHandler} value={value} handleOnSubmit={onSubmitHandler} handleKeyDown={handleKeyDown} isEdit={isEdit} />
   )
 
   const displayEmptyListForm = () => (
-    displayForm(titleValue, false)
+    <div className='card'>
+      <div className='card-boy my-5'>
+        {displayForm(titleValue, false)}
+      </div>
+    </div>
   );
 
   const displayEditTitleForm = (list) => (
@@ -22,11 +26,11 @@ export default function List({list, onDeleteHandler, onChangeHandler, titleValue
     return (
       <>
         <div 
-          className='card-title d-flex justify-content-center text-dark h-100 mb-0' 
+          className='card-title d-flex justify-content-center text-dark h-100 my-5' 
           onClick={handleShowTitleForm} 
           onKeyDown={handleKeyDown}
         >
-          {list.showTitleForm ? displayEditTitleForm(list) : <span className='card-title-text cursor-pointer pt-5 pb-4'>{list.title}</span>}
+          {list.showTitleForm ? displayEditTitleForm(list) : <span className='card-title-text cursor-pointer'>{list.title}</span>}
         </div>
         <div className='card-text'>
           <ul className="list-group">
@@ -34,12 +38,23 @@ export default function List({list, onDeleteHandler, onChangeHandler, titleValue
                 list.items.map(item => 
                   <Item 
                     item={item} 
+                    listId = {list._id}
                     onChangeHandler={(e) => itemOnChangeHandler(e, item._id)} 
                     onDeleteHandler={(e) => itemOnDeleteHandler(e, item._id)} 
-                    key={item._id}   
+                    onCheckHandler={(e) => itemOnCheckHandler(e, item._id)}
+                    onSubmitHandler={(e) => itemOnSubmitHandler(e, list._id, item.name)}
+                    handleKeyDown={(e) => handleKeyDown(e, list._id)}
+                    key={item._id ?? 'newly-added-item'}   
+                    itemNameValue={itemNameValue}
                   />
                 )
               }
+              <div className='list-group-item'>
+                <span className='cursor-pointer d-flex align-items-center hoverable' onClick={(e) => addNewItemHandler(e, list._id)}>
+                  <FontAwesomeIcon className='mr-2' icon={faCirclePlus} style={{ fontSize: "1.5rem" }} />
+                  <strong>Add Item</strong>
+                </span>
+              </div>
             </ul>
         </div>
       </>
@@ -50,7 +65,7 @@ export default function List({list, onDeleteHandler, onChangeHandler, titleValue
     <div className='col-md-3 my-3'>
       <div className='card' onMouseLeave={handleOnMouseLeave}>
         <div className='card--header position-relative text-danger'>
-          <FontAwesomeIcon className='position-absolute delete-icon cursor-pointer' icon={faTrashCan} onClick={onDeleteHandler} />
+          <FontAwesomeIcon className='position-absolute delete-icon cursor-pointer hoverable' icon={faTrashCan} onClick={onDeleteHandler} />
         </div>
         {checkIfObjEmpty(list) ? displayEmptyListForm() : displayExistingList()}
       </div>

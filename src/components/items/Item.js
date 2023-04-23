8bb845a65157ee556afb9d458aae2070e3632074
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-23 15:39:07 +0300

    adding empty item display, in case the user wants to add a new item to a list

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import Button from '../common/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { checkIfObjEmpty } from '../../utilities';
+import Form from '../common/Form';
 
-export default function Item ({item, onChangeHandler, onDeleteHandler}) {
-
+export default function Item ({item, onChangeHandler, onDeleteHandler, onSubmitHandler, handleKeyDown, itemNameValue}) {
   return (
-    <div className="list-group-item d-flex">
-        <span className={'mr-3 ' + (item.done ? 'text-decoration-line-through' : '')}>{item.name}</span>
-        <div className='mr-auto position-relative hoverable'>
-          <input className='item-check-box opacity-0 cursor-pointer' type="checkbox" onChange={onChangeHandler} checked={item.done}  />
-          <FontAwesomeIcon icon={faCheck} className={item.done ? 'text-success' : ''} />
-        </div>
-        {/* <Button className="btn-primary mr-3" onClickHandler={() => history.push(`/edititem/${item._id}`)} innerText="Edit" /> */}
-        <FontAwesomeIcon className='delete-icon cursor-pointer text-danger' icon={faXmark} onClick={onDeleteHandler} />
+    <div className="list-group-item d-flex align-items-center">
+      {checkIfObjEmpty(item) ? 
+        <Form handleOnChange={onChangeHandler} value={item.name ?? itemNameValue} handleOnSubmit={onSubmitHandler} handleKeyDown={handleKeyDown} />
+      :
+        <>
+          <span className={'mr-3 ' + (item.done ? 'text-decoration-line-through' : '')}>{item.name}</span>
+          <div className='mr-auto position-relative hoverable'>
+            <input className='item-check-box opacity-0 cursor-pointer' type="checkbox" onChange={onChangeHandler} checked={item.done}  />
+            <FontAwesomeIcon icon={faCheck} className={'hoverable ' + (item.done ? 'text-success' : '')} />
+          </div>
+          {/* <Button className="btn-primary mr-3" onClickHandler={() => history.push(`/edititem/${item._id}`)} innerText="Edit" /> */}
+          <FontAwesomeIcon className='delete-icon cursor-pointer text-danger hoverable' icon={faXmark} onClick={onDeleteHandler} />
+        </>
+      }
     </div>
   )
 }

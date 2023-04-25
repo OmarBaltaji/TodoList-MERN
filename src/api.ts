eeff4bbf926f85269d02481fceeb0118cdf1e1f2
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-26 00:40:59 +0300

    defined the return types of axios requests

--- src/api.ts
+++ src/api.ts
@@ -1,24 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
-import { ListResponse } from "./models";
-import { ListFormData, ItemFormData } from './models';
+import { ListFormData, ItemFormData, AllListsResponse, ListResponse, AllItemsResponse, ItemResponse } from './models';
 const baseUrl = 'http://localhost:5000/';
 const itemApiUrl = baseUrl + 'api/v1/items/';
 const listApiUrl = baseUrl + 'api/v1/lists/';
 
-
 export default {
   // Lists
-  getAllLists: () => axios.get(listApiUrl),
+  getAllLists: () => axios.get<AllListsResponse>(listApiUrl),
   createList: (formData: ListFormData) => axios.post<ListResponse>(listApiUrl, { ...formData }),
-  getList: (id: string) => axios.get(`${listApiUrl}${id}`),
-  deleteList: (id: string) => axios.delete(`${listApiUrl}${id}`),
-  updateList: (id: string, formData: ListFormData) => axios.patch(`${listApiUrl}${id}`, { ...formData }),
+  getList: (id: string) => axios.get<ListResponse>(`${listApiUrl}${id}`),
+  deleteList: (id: string) => axios.delete<string>(`${listApiUrl}${id}`),
+  updateList: (id: string, formData: ListFormData) => axios.patch<ListResponse>(`${listApiUrl}${id}`, { ...formData }),
 
   //Items
-  getListItems: (listId: string) => axios.post(itemApiUrl, { listId }),
-  createItem: (formData: ItemFormData) => axios.post(`${itemApiUrl}add`, { ...formData }),
-  getItem: (id: string) => axios.get(`${itemApiUrl}${id}`),
-  deleteItem: (id: string) => axios.delete(`${itemApiUrl}${id}`),
-  updateItem: (id: string, formData: ItemFormData) => axios.patch(`${itemApiUrl}${id}`, { ...formData }),
+  getListItems: (listId: string) => axios.post<AllItemsResponse>(itemApiUrl, { listId }),
+  createItem: (formData: ItemFormData) => axios.post<ItemResponse>(`${itemApiUrl}add`, { ...formData }),
+  getItem: (id: string) => axios.get<ItemResponse>(`${itemApiUrl}${id}`),
+  deleteItem: (id: string) => axios.delete<string>(`${itemApiUrl}${id}`),
+  updateItem: (id: string, formData: ItemFormData) => axios.patch<ItemResponse>(`${itemApiUrl}${id}`, { ...formData }),
 }

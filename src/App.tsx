20a0b471bad845caa9d3dc9b1523a39f7537714a
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-25 20:13:51 +0300

    installed typescript and reformatted index and App to take the tsx extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import * as React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './components/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Router>

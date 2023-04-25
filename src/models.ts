3a54f4af6030d84bd9b79e0a4646cf12dea7b938
Omar Baltaji <omr.baltaji@gmail.com>
2023-04-26 00:23:08 +0300

    adding more types

--- src/models.ts
+++ src/models.ts
@@ -1,14 +1,32 @@
 export interface ListObject {
-  _id: string;
-  title: string;
+  _id?: string;
+  title?: string;
   items?: ItemObject[];
   showTitleForm?: boolean; 
 }
 
 export interface ItemObject {
-  _id: string;
-  name: string;
-  done: boolean;
-  listId: string;
+  _id?: string;
+  name?: string;
+  done?: boolean;
+  listId?: string;
   showNameForm?: boolean;
+}
+
+export interface ListResponse {
+  list: ListObject
+}
+
+export interface ListFormData {
+  title: string;
+}
+
+export interface ItemFormData {
+  name?: string;
+  listId?: string;
+  done?: boolean;
+}
+
+export interface ItemResponse {
+  item: ItemObject
 }

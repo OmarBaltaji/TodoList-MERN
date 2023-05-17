532d7ca34dd69b3911d661782dbf1ccdeae03209
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-17 23:33:46 +0300

    Added client connector to the App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import clientGraphql from './client.graphql';
+import { ApolloProvider } from '@apollo/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={clientGraphql}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

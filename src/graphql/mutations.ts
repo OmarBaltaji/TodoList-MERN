5e768f76295c4bff5cdb365329fff30096dae6d2
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-18 00:15:15 +0300

    Modified update list mutation so done property is returned on items. Added the Create Update and Delete mutations for Item

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_LIST = gql`
-  mutation CreateList($dto: ListDto!) {
+  mutation CreateList ($dto: ListDto!) {
     createList (dto: $dto) {
       list {
         _id,
@@ -16,14 +16,15 @@ export const CREATE_LIST = gql`
 `;
 
 export const UPDATE_LIST = gql`
-  mutation UpdateList($id: String!, $dto: ListDto!) {
+  mutation UpdateList ($id: String!, $dto: ListDto!) {
     updateList (id: $id, dto: $dto) {
       list {
         _id,
         title,
         items {
           _id,
-          name
+          name,
+          done
         }
       }
     }
@@ -31,7 +32,37 @@ export const UPDATE_LIST = gql`
 `;
 
 export const DELETE_LIST = gql`
-  mutation DeleteList($id: String!) {
+  mutation DeleteList ($id: String!) {
     deleteList (id: $id)
+  }
+`;
+
+export const CREATE_ITEM = gql`
+  mutation CreateItem ($dto: CreateItemDto!) {
+    createItem (dto: $dto) {
+      item {
+        _id,
+        name,
+        done
+      }
+    }
+  }
+`;
+
+export const UPDATE_ITEM = gql`
+  mutation UpdateItem ($id: String!, $dto: EditItemDto!) {
+    updateItem (id: $id, dto: $dto) {
+      item {
+        _id,
+        name,
+        done
+      }
+    }
+  }
+`;
+
+export const DELETE_ITEM = gql`
+  mutation DeleteItem ($id: String!) {
+    deleteItem (id: $id)
   }
 `;

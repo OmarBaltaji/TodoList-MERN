352e181ae7c42f4919358e8def0277ddcfdb15fb
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-18 00:12:32 +0300

    Added CreateItemDto and EditItemDto types

--- src/graphql/input.type.ts
+++ src/graphql/input.type.ts
@@ -1,3 +1,14 @@
 export interface ListDto {
   title: string;
 }
+
+export interface CreateItemDto {
+  name: string;
+  listId: string | undefined;
+}
+
+export interface EditItemDto {
+  name?: string;
+  listId?: string;
+  done?: boolean;
+}

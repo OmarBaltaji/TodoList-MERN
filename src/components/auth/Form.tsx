7611e5f028fc8403c8b33525260b4a1780f14d88
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-29 01:06:26 +0300

    Removed disable button, instead checking if one of the fields contain an error, in which case no api call is made.

--- src/components/auth/Form.tsx
+++ src/components/auth/Form.tsx
@@ -16,31 +16,24 @@ interface Props {
 }
 
 const Form: React.FC<Props> = ({ page, authData, setAuthData, formErrors, setFormErrors, authMutation }) => {
-  const [disableSubmit, setDisableSubmit] = useState<boolean>(true);
   const history = useHistory();
 
   useEffect(() => {
-    // If all inputs are filled, check if one of the inputs contain an error. In case yes, then disable submit button
-    if (Object.values(authData).every(input => input))
-      setDisableSubmit(() => Object.values(formErrors).some(error => error !== ''));
-
     const isLoggedIn: string = localStorage.getItem('isLoggedIn') ?? '0';
     if (parseInt(isLoggedIn))
       history.push('/home');
-  }, [formErrors]);
+  }, []);
 
   const submitForm = async (e: React.FormEvent<EventTarget>) => {
     e.preventDefault();
 
-    const areAllFilled: boolean = Object.values(authData).every(data => data !== '');
-
-    if(areAllFilled) {
+    const doErrorsExist = Object.values(formErrors).some(error => error !== '');
+    if(!doErrorsExist) {
       try {
         const { data } = await authMutation({ variables: { dto: authData } });
         const result: boolean = page === 'login' ? data.login.result : data.register.result;
 
         if(result) {
-          setDisableSubmit(true);
           if (page === 'login') {
             localStorage.setItem('isLoggedIn', '1');
             setAuthData(() => ({ email: '', password: '' }));
@@ -60,7 +53,7 @@ const Form: React.FC<Props> = ({ page, authData, setAuthData, formErrors, setFor
         toast.error(error.graphQLErrors[0].message);
       }
     } else {
-      toast.error('All fields must be filled');
+      toast.error("Please provide valid credentials");
     }
   }
 
@@ -111,7 +104,7 @@ const Form: React.FC<Props> = ({ page, authData, setAuthData, formErrors, setFor
                       :
                       <div>Already have an account? <Link to='/'>Login</Link></div>
                     }
-                    <button className="btn btn-primary btn py-2 px-4" onClick={submitForm} disabled={disableSubmit}>Submit</button>
+                    <button className="btn btn-primary btn py-2 px-4" onClick={submitForm}>Submit</button>
                 </div>
               </form>
             </div>

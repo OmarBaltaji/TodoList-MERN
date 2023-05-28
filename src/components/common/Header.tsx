2b647b7f22c6a8cd1b1453a959967598d7d2d323
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-28 17:35:51 +0300

    Placed the ToastContainer in the header to only call it in one place. Modified the logout functionality to hide the Logout button in case the user is already logged out

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,28 +1,33 @@
 import { useMutation } from '@apollo/client';
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import { LOGOUT } from '../../graphql/mutations';
+import { ToastContainer } from 'react-toastify';
 
 const Header: React.FC = () => {
   const history = useHistory();
-  const isLoggedIn = localStorage.getItem('isLoggedIn');
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') ?? '0');
   const [logoutMutation, {}] = useMutation(LOGOUT);
 
   const logout = async () => {
-    const { data : { logout: { result } } } = await logoutMutation();
+    const { data: { logout: { result } } } = await logoutMutation();
     if(result) {
+      setIsLoggedIn('0');
+      localStorage.removeItem('isLoggedIn');
       history.push('/');
-      localStorage.removeItem('isLoggedIn');    
     }
   }
 
   return (
-    <nav className="navbar navbar-light bg-light shadow-sm">
-      <div className="container-fluid">
-        <span className="navbar-brand">TodoList</span>
-        {isLoggedIn && <button className="btn btn-danger" onClick={logout}>Logout</button>}
-      </div>
-    </nav>
+    <>
+      <ToastContainer autoClose={3000} />
+      <nav className="navbar navbar-light bg-light shadow-sm">
+        <div className="container-fluid">
+          <span className="navbar-brand">TodoList</span>
+          {isLoggedIn !== '0' && <button className="btn btn-danger" onClick={logout}>Logout</button>}
+        </div>
+      </nav>
+    </>
   )
 }
 
